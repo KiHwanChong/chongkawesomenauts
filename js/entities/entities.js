d3cc14774227286641e6e5cb0b7c272fd1e922a4
Ki Hwan Chong <joelkh784@gmail.com>
2015-03-05 11:19:02 -0800

    no message

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -1,1 +1,31 @@
-// TODO
+game.PlayerEntity = me.Entity.extend({
+    init: function(x, y, settings){
+     //loading player with 64*64 size
+        this._super(me.Entity, 'init', [x, y, {
+            image: "player",
+            width: 64,
+            height: 64,
+            spritewidth: "64",
+            spriteheight: "64",
+            getShape: function(){
+                return(new me. Rect(0, 0, 64, 64)).toPolygon();
+            }
+        }]);
+    
+        this.body.setVelocity(5, 20);
+    
+    },
+    
+    update: function(delta){
+        if(me.input.isKeyPressed("right")){
+            //adds to the position of my x by adding the velocity defined above in
+            //setVelocity() and multiplying it by me.timer tick.
+            //me.timer.tick makes the movements look smooth
+            this.body.vel.x += this.bodyaccel.x * me.timer.tick;
+        } else{
+            this.body.vel.x = 0;
+        }
+        this.body.update(delta);
+        return true;
+    }
+});
